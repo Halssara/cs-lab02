122abafeea32e1bf9a9c78c6e86dfe4ebbca8411
Alice (IvanovOB) <IvanovOB@mpei.ru>
2021-03-22 11:52:46 +0300

    деление

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
 cout << "Enter A and B: ";
-int a, b;
+float a, b;
 cin >> a >> b;
-cout << "A + B = " << a + b << '\n' << "A - B = " << a - b << '\n' << "A * B =" << a * b << '\n';
+cout << "A + B = " << a + b << '\n' << "A - B = " << a - b << '\n' << "A * B =" << a * b << '\n'
+<< "A / b = " << a / b * 1.0 << '\n';
 }
