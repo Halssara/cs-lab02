05182c280b44af6f69beadefb31142dbdc301b77
Alice (IvanovOB) <IvanovOB@mpei.ru>
2021-03-22 12:24:43 +0300

    Double

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 cout << "Enter A and B: ";
-float a, b;
+double a, b;
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n' << "A - B = " << a - b << '\n' << "A * B =" << a * b << '\n'
 << "A / b = " << a / b * 1.0 << '\n';
