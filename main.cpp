6dbd5466642db30aeb2f2b8caead5964244dd104
Bob <BobTI@gmail.com>
2021-03-22 12:11:08 +0300

    минимум

--- main.cpp
+++ main.cpp
@@ -9,9 +9,18 @@ float a, b;
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n' << "A - B = " << a - b << '\n' << "A * B =" << a * b << '\n'
 << "A / b = " << a / b * 1.0 << '\n';
+
 float max;
 if (a < b)
     max = b;
 else max = a;
 cout << max;
+
+float min;
+if (a < b)
+    min = a;
+else
+    min = b;
+cout << min;
+
 }
