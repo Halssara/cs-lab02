a729456815848efe25978de3fa9b0c5580dfe115
Alice (IvanovOB) <IvanovOB@mpei.ru>
2021-03-22 12:08:52 +0300

    Поиск максимума

--- main.cpp
+++ main.cpp
@@ -9,4 +9,9 @@ float a, b;
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n' << "A - B = " << a - b << '\n' << "A * B =" << a * b << '\n'
 << "A / b = " << a / b * 1.0 << '\n';
+float max;
+if (a < b)
+    max = b;
+else max = a;
+cout << max;
 }
